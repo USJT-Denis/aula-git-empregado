fd7634208fb28f7c48a17108412d2441b08918f7
USJT-Denis <denissantos.3654@aluno.saojudas.br>
2021-09-14 19:05:40 -0300

    Incluindo arquivos faltantes do commit anterior.

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,5 +1,20 @@
+public class TesteEmpregado{
+public static void main (String [] args){
+Empregado e1 = new Empregado ();
+Empregado e2 = new Empregado();
+Empregado e3 = new Empregado ();
+//e agora, como configurar o tipo deles?
 
 public class TesteEmpregado{
 public static void main (String [] args){
+Empregado e1 = new Empregado ();
+Empregado e2 = new Empregado();
+Empregado e3 = new Empregado ();
+//configurando tipo dos empregados
+e1.setTipo(1);
+e2.setTipo(2);
+e3.setTipo(3);
+//configurando alguns valores
+e1.setSalario(2000);
 }
 }
