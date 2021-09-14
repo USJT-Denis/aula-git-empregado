2f9dcd66aee2023be33542c2a39399373a0019a5
USJT-Denis <denissantos.3654@aluno.saojudas.br>
2021-09-14 19:08:02 -0300

    alt

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -16,5 +16,13 @@ e2.setTipo(2);
 e3.setTipo(3);
 //configurando alguns valores
 e1.setSalario(2000);
+e2.setSalario(1700);
+e2.setComissao (0.2);//20% de comissão sobre o salário
+e3.setSalario(1500);
+e3.setBonus(400);
+//mostra os valores
+System.out.println (e1.calculaSalario());
+System.out.println (e2.calculaSalario());
+System.out.println
 }
 }
